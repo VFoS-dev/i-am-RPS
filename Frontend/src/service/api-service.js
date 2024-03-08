b86a6cd2907f7bd45dd8f4c6afbe60d6dfdc0a22
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2024-03-07 18:15:44 -0700

    updated api calls

--- Frontend/src/service/api-service.js
+++ Frontend/src/service/api-service.js
@@ -1,11 +1,31 @@
 import { env } from "@/_helper/env";
 import { io } from "socket.io-client";
+
 const API = env('VITE_API', 'http://localhost:3000')
 const socket = io(API);
 
 socket.on('gameUpdated', gameUpdated)
+socket.on('notify', notify)
+socket.on('kickFromLobby', kickFromLobby)
 
-export async function gameUpdated(data) {
+const gameId = 1 // fix later
+
+export async function checkConnection() {
+    const data = await (await fetch(API)).json()
+    if (!data.success) {
+        console.error('Api failed to connect');
+    }
+}
+
+function notify(data) {
+    console.log('notify', data)
+}
+
+function kickFromLobby(data) {
+    console.log('kickFromLobby', data)
+}
+
+function gameUpdated(data) {
     console.log('gameUpdated', data);
 }
 
@@ -17,23 +37,20 @@ export async function gameJoin(gameCode) {
     socket.emit('game_join', { gameCode }, HandleAPIGame)
 }
 
-export async function gameKickPlayer() {
-    socket.emit('game_kickPlayer', {}, HandleAPIGame)
+export async function gameKickPlayer(playerId) {
+    socket.emit('game_kickPlayer', { gameId, playerId }, HandleAPIGame)
 }
 
-export async function gameIAm() {
-    socket.emit('game_iAm', {}, HandleAPIGame)
+export async function gameIAm(prompt, image) {
+    socket.emit('game_iAm', { gameId, prompt, image }, HandleAPIGame)
+}
+
+export async function startGame() {
+    socket.emit('game_start', { gameId }, HandleAPIGame)
 }
 
 export async function googleImageSearch() {
     socket.emit('google_imageSearch', {}, HandleAPIGoogle)
-}
-
-export async function checkConnection() {
-    const data = await (await fetch(API)).json()
-    if (!data.success) {
-        console.error('Api failed to connect');
-    }
 }
 
 function HandleAPIGame({ error, success, ...rem } = {}) {
