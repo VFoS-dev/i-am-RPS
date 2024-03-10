3a1755d10cfbd3aebbcbad519d8f5f9761e34752
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2024-03-10 12:37:16 -0600

    prompt refinement

--- Backend/src/service/OpenAI.js
+++ Backend/src/service/OpenAI.js
@@ -9,18 +9,25 @@ async function AbeatsB(A, B) {
     const completion = await openai.chat.completions.create({
         messages: [{
             role: "system",
-            content: "the following is a set of directions each separated by \"-\"" +
-                " the first one starts with \"- in this game\" which is the game's description," +
-                " the second one starts with \"- A being:\"," +
-                " the third is \"- B being:\"," +
-                " if there are more than that return and error and the reason like this {\"error\":true, \"reason\" }\n" + 
-                " if you are to reply in response to A or B please use the respective input value instead\n" +
-                " respond in JSON format like { \"answer\", \"reason\"}, answer needs to be a boolean, if the two are equals please change answer to -1\n" +
-                " an example response would be \"{ \"answer\": true, \"reason\": \"A ferret would beat a flea by being able to catch and eat it.\" }\"\n" +
-                " please return the content as a one line string\n" +
-                "- in this game \"I am\" is like rock, paper, scissors, where any option is a allowed, not limited to rock, paper or scissors would A beat B? and how so?\n" +
-                `- A being: "${A}"\n` +
-                `- B being: "${B}"`
+            content: `
+                the following is a set of directions each separated by "-"
+                the first one starts with "- your behaviour:" which is how you need to act and respond
+                the second one starts with "- in this game:" which is the game's description
+                the third one starts with "- A being:"
+                the forth is "- B being:"
+            
+                - your behaviour:
+                respond in JSON format like { "answer", "equals": false, "reason" }, answer needs to be a boolean,
+                if the two are equals return { "answer": true, "equals": true, "reason" } instead,
+                if there are more than that return an error and its reason like: { "error": true, "reason" },
+                the response needs to be as a one line string,
+                if you are to reply in response to A or B use the respective input value instead.
+            
+                - in this game: "I am", which is similar to rock, paper, scissors, where any option is a allowed, not limited to rock, paper or scissors would A beat B? and how so?
+                - A being: "${A}"
+                - B being: "${B}"
+            `.split('\n').map(str => str.trim()).join('\n')
+
         }],
         model: "gpt-3.5-turbo",
     });
