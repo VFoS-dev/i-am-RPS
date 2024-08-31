78e92badf067e7f1bf49f7fe4fce3da5e19058fc
Jon Kido <46792496+VFoS-dev@users.noreply.github.com>
2024-08-31 15:19:48 -0600

    backend adjustments
    Update package-lock.json

--- Backend/src/_helper/db.js
+++ Backend/src/_helper/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { env } = require('./env');
 
-mongoose.connect(env("MongoDB_URL", 'mongodb://127.0.0.1:27017/test')).then(() => {
+mongoose.connect(env("MongoDB_URL", 'mongodb://127.0.0.1:27017/i-am-RPS')).then(() => {
   console.log("Connected to Database");
 }).catch((err) => {
   console.log("Not Connected to Database ERROR! ", err);
--- Backend/src/_helper/error.js
+++ Backend/src/_helper/error.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose')
+const { ValidationError } = mongoose.Error;
+
 class InvalidAttempt extends Error {
     constructor(message, title = "Invalid Attempt") {
         super(message);
@@ -5,19 +8,40 @@ class InvalidAttempt extends Error {
     }
 }
 
+function parseMongoError(errorObject) {
+    let errorMessages = [];
+
+    for (const field in errorObject) {
+        if (errorObject.hasOwnProperty(field)) {
+            const errorDetail = errorObject[field];
+
+            const message = errorDetail.properties && errorDetail.properties.message
+                ? errorDetail.properties.message
+                : `Invalid input for field: ${field}`;
+
+            errorMessages.push(message);
+        }
+    }
+
+    return errorMessages.join(', ');
+}
+
 function errorHandler(error) {
+    const Error = (extra = {}) => ({ error: true, ...extra })
     switch (error.constructor) {
         case InvalidAttempt:
-            return {
-                error: true,
+            return Error({
                 title: error.title,
                 message: error.message
-            }
+            })
+        case ValidationError:
+            return Error({
+                title: 'Validation Error',
+                message: parseMongoError(error.errors)
+            })
         default:
             console.log('💥Uncaught Error Exeption:\n', error);
-            return {
-                error: true
-            }
+            return Error()
     }
 }
 
